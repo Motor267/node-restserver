e388a97709c950989de3b9199e7fb27dd495efe5
Angel <angel.14.98@hotmail.com>
2020-09-25 08:52:13 -0500

    Variable de enterno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://motor:PsMGzW7xRnbrNhsN@cluster0.bstku.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
